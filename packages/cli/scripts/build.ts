de0c7e198dc28938dc8158edbc9ed5931190adeb
RetoRuto9900K <tutinoko2048@gmail.com>
2025-12-01 00:01:54 +0900

    update(utils): bump verson to 0.1.0-beta

--- packages/cli/scripts/build.ts
+++ packages/cli/scripts/build.ts
@@ -1,9 +1,12 @@
 import packageJson from "../package.json" with { type: "json" };
 
-Bun.build({
+await Bun.build({
   entrypoints: ["src/main.ts"],
   outdir: "dist",
   target: "node",
   format: "esm",
   external: Object.keys(packageJson.dependencies), // updater-coreを除外するため
 });
+
+console.log("Build completed.");
+
